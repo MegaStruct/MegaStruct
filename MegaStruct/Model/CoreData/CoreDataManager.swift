5cb68258042934d47130ab5081a14b89d8038a8b
KJH <gh7052@gmail.com>
2024-04-23 21:24:05 +0900

    Add : CoreDataManager 추가

---
+++ MegaStruct/Model/CoreData/CoreDataManager.swift
@@ -0,0 +1,16 @@
+//
+//  CoreDataManager.swift
+//  MegaStruct
+//
+//  Created by 김정호 on 4/23/24.
+//
+
+import Foundation
+
+final class CoreDataManager {
+    
+    // MARK: - properties
+    
+    // MARK: - methods
+    
+}
