9d6303a1a1a267226e70f5080c6bea1568adebe7
KJH <gh7052@gmail.com>
2024-04-23 21:19:09 +0900

    Add : UIColor+Extension 추가

---
+++ MegaStruct/Extension/UIColor+Extension.swift
@@ -0,0 +1,18 @@
+//
+//  UIColor+Extension.swift
+//  MegaStruct
+//
+//  Created by 김정호 on 4/23/24.
+//
+
+import UIKit
+
+extension UIColor {
+    static var megaRed: UIColor {
+        return .init(red: 245/225, green: 78/255, blue: 78/255, alpha: 1)
+    }
+    
+    static var whiteGray: UIColor {
+        return .init(red: 246/225, green: 246/255, blue: 246/255, alpha: 1)
+    }
+}
