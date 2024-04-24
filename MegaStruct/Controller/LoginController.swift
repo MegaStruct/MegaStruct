6a9ed277a0eec0aa273b4a6b263e551ad0e120bb
Yeom-A-Hyeon <20180982@dongduk.ac.kr>
2024-04-24 17:44:45 +0900

    Add : Login UI and feature create

--- MegaStruct/Controller/LoginController.swift
+++ MegaStruct/Controller/LoginController.swift
@@ -9,21 +9,61 @@ import UIKit
 
 class LoginController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    var userModel = UserModel() // 모델 인스턴스 생성
+    //버튼 넣기 userNameTextField: UITextField
+    @IBOutlet weak var userIdTextField: UITextField!
+    //버튼 넣기 userPWDTextField: UITextField
+    @IBOutlet weak var userPWDTextField: UITextField!
+    //버튼 넣기 loginBtn: RoundBtn
+    @IBOutlet weak var loginBtn: UIButton!
+    
+    func customTextField(_textField: UITextField) {
+        //텍스트 필드 둥글게
+        _textField.layer.cornerRadius = 25.0
+        _textField.layer.masksToBounds = true
+        
+        //텍스트 필드 안에 패딩
+        _textField.leftView = UIView(frame: CGRect(x: 0.0, y: 0.0, width: 16.0, height: 0.0))
+        _textField.leftViewMode = .always
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        customTextField(_textField: userIdTextField)
+        customTextField(_textField: userPWDTextField)
     }
-    */
+    
+//    //아이디 형식 검사
+//    func isValidEmail(id: String) -> Bool {
+//            let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}"
+//            let emailTest = NSPredicate(format: "SELF MATCHES %@", emailRegEx)
+//            return emailTest.evaluate(with: id)
+//        }
+//        
+//        // 비밀번호 형식 검사
+//        func isValidPassword(pwd: String) -> Bool {
+//            let passwordRegEx = "^[a-zA-Z0-9]{8,}$"
+//            let passwordTest = NSPredicate(format: "SELF MATCHES %@", passwordRegEx)
+//            return passwordTest.evaluate(with: pwd)
+//        }
+//
 
+    @IBAction func loginBtnOnClick(_ sender: Any) {
+        
+        guard let userId = userIdTextField.text, !userId.isEmpty else {return}
+        guard let userPWD = userPWDTextField.text, !userPWD.isEmpty else {return}
+        
+        let loginSuccess : Bool = userModel.hasUser(name: userId, pwd: userPWD)
+        if loginSuccess {
+            guard let nextVC = self.storyboard?.instantiateViewController(identifier: "TemporaryMainViewController") else {return}
+            self.present(nextVC, animated: true)
+        }
+        
+    }
+    
+    @IBAction func autoLoginOnClick(_ sender: Any) {
+        
+    }
 }
--- MegaStruct/Model/User/UserDataManager.swift
+++ MegaStruct/Model/User/UserDataManager.swift
@@ -6,3 +6,38 @@
 //
 
 import Foundation
+
+// MARK: - properties
+final class UserModel {
+    struct User {
+        var userId: String
+        var userPWD: String
+    }
+    
+    var model: [User] = [
+        User (userId: "ahyeon", userPWD: "1234"),
+        User (userId: "jeongho", userPWD: "5678"),
+        User (userId: "jiyeon", userPWD: "91011"),
+        User (userId: "hanbit", userPWD: "12131415")
+    ]
+    
+    // MARK: - methods
+    
+    //이미 있는 유저인지 확인
+    func hasUser (name: String, pwd: String) -> Bool {
+        for user in model {
+            if user.userId == name && user.userPWD == pwd {
+                return true
+                
+            }
+        }
+        return false
+    }
+    
+    //새로운 유저 추기
+    func addUser(name: String, pwd: String) {
+        let newUser = User(userId: name, userPWD: pwd)
+        model.append(newUser)
+    }
+    
+}
