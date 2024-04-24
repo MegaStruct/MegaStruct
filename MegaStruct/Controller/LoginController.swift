84d9acf0825ce260cf7bb9109a974c038b8b06df
Yeom-A-Hyeon <20180982@dongduk.ac.kr>
2024-04-24 23:18:20 +0900

    Add : AutoLogin checkmark

--- MegaStruct/Controller/LoginController.swift
+++ MegaStruct/Controller/LoginController.swift
@@ -17,6 +17,22 @@ class LoginController: UIViewController {
     //버튼 넣기 loginBtn: RoundBtn
     @IBOutlet weak var loginBtn: UIButton!
     
+    @IBOutlet weak var checkBoxImageView: UIImageView!
+    
+    @IBOutlet weak var autoLoginBtn: UIButton!
+    
+    
+    @objc func checkBoxDidTap() {
+        if checkBoxImageView.image == UIImage(systemName: "square") {
+                checkBoxImageView.image = UIImage(systemName: "checkmark.square")
+              } else {
+                checkBoxImageView.image = UIImage(systemName: "square")
+              }
+    }
+    
+    
+    
+    
     func customTextField(_textField: UITextField) {
         //텍스트 필드 둥글게
         _textField.layer.cornerRadius = 25.0
@@ -25,7 +41,6 @@ class LoginController: UIViewController {
         //텍스트 필드 안에 패딩
         _textField.leftView = UIView(frame: CGRect(x: 0.0, y: 0.0, width: 16.0, height: 0.0))
         _textField.leftViewMode = .always
-        
     }
     
     
@@ -33,23 +48,15 @@ class LoginController: UIViewController {
         super.viewDidLoad()
         customTextField(_textField: userIdTextField)
         customTextField(_textField: userPWDTextField)
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(checkBoxDidTap))
+                checkBoxImageView.isUserInteractionEnabled = true
+                checkBoxImageView.addGestureRecognizer(tapGesture) // 이미지 뷰 타겟
+        
     }
     
-//    //아이디 형식 검사
-//    func isValidEmail(id: String) -> Bool {
-//            let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}"
-//            let emailTest = NSPredicate(format: "SELF MATCHES %@", emailRegEx)
-//            return emailTest.evaluate(with: id)
-//        }
-//        
-//        // 비밀번호 형식 검사
-//        func isValidPassword(pwd: String) -> Bool {
-//            let passwordRegEx = "^[a-zA-Z0-9]{8,}$"
-//            let passwordTest = NSPredicate(format: "SELF MATCHES %@", passwordRegEx)
-//            return passwordTest.evaluate(with: pwd)
-//        }
-//
-
+    
+    
     @IBAction func loginBtnOnClick(_ sender: Any) {
         
         guard let userId = userIdTextField.text, !userId.isEmpty else {return}
@@ -59,11 +66,12 @@ class LoginController: UIViewController {
         if loginSuccess {
             guard let nextVC = self.storyboard?.instantiateViewController(identifier: "TemporaryMainViewController") else {return}
             self.present(nextVC, animated: true)
+            //TemporaryMainViewController 는 정호님 메인 페이지랑 연결할거라 확인해보려고 만든 컨트롤러!
         }
         
     }
+ 
     
-    @IBAction func autoLoginOnClick(_ sender: Any) {
-        
-    }
+    
 }
+
