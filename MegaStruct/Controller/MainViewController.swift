521aac5e28e8b18e04aabd739c1895eb49022c64
KJH <gh7052@gmail.com>
2024-04-26 01:14:07 +0900

    Add : MainViewController 데이터 및 컬렉션 뷰 구성 기능 추가

--- MegaStruct/Controller/MainViewController.swift
+++ MegaStruct/Controller/MainViewController.swift
@@ -7,23 +7,229 @@
 
 import UIKit
 
-class MainViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+final class MainViewController: UIViewController {
+    
+    // MARK: - properties
+    private let mainView = MainView()
+    
+    private let networkManager = NetworkManager.shared
+    
+    private var upcomingMovies = [Movie]()
+    private var nowPlayingMovies = [Movie]()
+    private var popularMovies = [Movie]()
+    private var topRatedMovies = [Movie]()
+    
+    // MARK: - life cycles
+    override func loadView() {
+        view = self.mainView
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureData()
+        configureCollectionView()
     }
-    */
+    
+    // MARK: - methods
+    private func configureData() {
+        let dispatchGroup = DispatchGroup()
+        
+        fetchUpcomingMovieList(group: dispatchGroup)
+        fetchNowPlayingMovieList(group: dispatchGroup)
+        fetchPopularMovieList(group: dispatchGroup)
+        fetchTopRatedMovieList(group: dispatchGroup)
+        
+        dispatchGroup.notify(queue: .main) { [weak self] in
+            self?.mainView.collectionView.reloadData()
+        }
+    }
+    
+    private func fetchUpcomingMovieList(group: DispatchGroup) {
+        group.enter()
+        
+        networkManager.fetchMovieList(category: .upcoming, language: "ko", page: 1) { result in
+            switch result {
+            case .success(let data):
+                self.upcomingMovies = data.results
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+            group.leave()
+        }
+    }
+    
+    private func fetchNowPlayingMovieList(group: DispatchGroup) {
+        group.enter()
+        
+        networkManager.fetchMovieList(category: .nowPlaying, language: "ko", page: 1) { result in
+            switch result {
+            case .success(let data):
+                self.nowPlayingMovies = data.results
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+            group.leave()
+        }
+    }
+    
+    private func fetchPopularMovieList(group: DispatchGroup) {
+        group.enter()
+        
+        networkManager.fetchMovieList(category: .popular, language: "ko", page: 1) { result in
+            switch result {
+            case .success(let data):
+                self.popularMovies = data.results
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+            group.leave()
+        }
+    }
+    
+    private func fetchTopRatedMovieList(group: DispatchGroup) {
+        group.enter()
+        
+        networkManager.fetchMovieList(category: .topRated, language: "ko", page: 1) { result in
+            switch result {
+            case .success(let data):
+                self.topRatedMovies = data.results
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+            group.leave()
+        }
+    }
+    
+    private func configureCollectionView() {
+        mainView.collectionView.delegate = self
+        mainView.collectionView.dataSource = self
+        
+        mainView.collectionView.register(MainCollectionViewCell.self, forCellWithReuseIdentifier: "collectionViewCell")
+        mainView.collectionView.register(MainHeaderView.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: "MainHeaderView")
+    }
+}
 
+extension MainViewController: UICollectionViewDelegate {
+    
+}
+
+extension MainViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        switch kind {
+        case UICollectionView.elementKindSectionHeader:
+            guard let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "MainHeaderView", for: indexPath) as? MainHeaderView else {
+                return UICollectionReusableView()
+            }
+            
+            switch indexPath.section {
+            case 0:
+                header.prepare(text: "Upcoming")
+            case 1:
+                header.prepare(text: "Now playing")
+            case 2:
+                header.prepare(text: "Popular")
+            default:
+                header.prepare(text: "Top rated")
+            }
+            
+            return header
+        default:
+            return UICollectionReusableView()
+        }
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 4
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        switch section {
+        case 0:
+            return upcomingMovies.count
+        case 1:
+            return nowPlayingMovies.count
+        case 2:
+            return popularMovies.count
+        default:
+            return topRatedMovies.count
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "collectionViewCell", for: indexPath) as? MainCollectionViewCell else { return UICollectionViewCell() }
+        
+        switch indexPath.section {
+        case 0:
+            if let posterPath = upcomingMovies[indexPath.row].posterPath {
+                networkManager.fetchPosterUrlImage(url: posterPath) { result in
+                    switch result {
+                    case .success(let data):
+                        DispatchQueue.main.async {
+                            cell.bind(movie: self.upcomingMovies[indexPath.row], imageData: data)
+                        }
+                    case .failure(let error):
+                        DispatchQueue.main.async {
+                            cell.bind(movie: self.upcomingMovies[indexPath.row], imageData: nil)
+                        }
+                    }
+                }
+            } else {
+                cell.bind(movie: self.upcomingMovies[indexPath.row], imageData: nil)
+            }
+        case 1:
+            if let posterPath = nowPlayingMovies[indexPath.row].posterPath {
+                networkManager.fetchPosterUrlImage(url: posterPath) { result in
+                    switch result {
+                    case .success(let data):
+                        DispatchQueue.main.async {
+                            cell.bind(movie: self.nowPlayingMovies[indexPath.row], imageData: data)
+                        }
+                    case .failure(let error):
+                        DispatchQueue.main.async {
+                            cell.bind(movie: self.nowPlayingMovies[indexPath.row], imageData: nil)
+                        }
+                    }
+                }
+            } else {
+                cell.bind(movie: self.nowPlayingMovies[indexPath.row], imageData: nil)
+            }
+        case 2:
+            if let posterPath = popularMovies[indexPath.row].posterPath {
+                networkManager.fetchPosterUrlImage(url: posterPath) { result in
+                    switch result {
+                    case .success(let data):
+                        DispatchQueue.main.async {
+                            cell.bind(movie: self.popularMovies[indexPath.row], imageData: data)
+                        }
+                    case .failure(let error):
+                        DispatchQueue.main.async {
+                            cell.bind(movie: self.popularMovies[indexPath.row], imageData: nil)
+                        }
+                    }
+                }
+            } else {
+                cell.bind(movie: self.popularMovies[indexPath.row], imageData: nil)
+            }
+        default:
+            if let posterPath = topRatedMovies[indexPath.row].posterPath {
+                networkManager.fetchPosterUrlImage(url: posterPath) { result in
+                    switch result {
+                    case .success(let data):
+                        DispatchQueue.main.async {
+                            cell.bind(movie: self.topRatedMovies[indexPath.row], imageData: data)
+                        }
+                    case .failure(let error):
+                        DispatchQueue.main.async {
+                            cell.bind(movie: self.topRatedMovies[indexPath.row], imageData: nil)
+                        }
+                    }
+                }
+            } else {
+                cell.bind(movie: self.topRatedMovies[indexPath.row], imageData: nil)
+            }
+        }
+        
+        return cell
+    }
 }
