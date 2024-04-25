8ae1b18c2a552aa45de90ac5033401ed9ae71606
KJH <gh7052@gmail.com>
2024-04-26 02:19:09 +0900

    Design : MainFooterView UI 구성

--- MegaStruct/View/MainFooterView.swift
+++ MegaStruct/View/MainFooterView.swift
@@ -6,7 +6,33 @@
 //
 
 import UIKit
+import SnapKit
 
-class MainFooterView: UICollectionReusableView {
+final class MainFooterView: UICollectionReusableView {
+    
+    // MARK: - properties
+    private let divisionLineView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .megaRed
+        return view
+    }()
+    
+    // MARK: - methods
+    override init(frame: CGRect) {
+        super.init(frame: frame)
         
+        configureConstraint()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configureConstraint() {
+        self.addSubview(divisionLineView)
+        
+        divisionLineView.snp.makeConstraints {
+            $0.top.leading.trailing.bottom.equalToSuperview()
+        }
+    }
 }
