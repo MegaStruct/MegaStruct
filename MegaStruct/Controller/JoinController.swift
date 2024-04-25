ba1d73822ef3db286df6903b0da227350a11d93f
Yeom-A-Hyeon <20180982@dongduk.ac.kr>
2024-04-25 11:37:43 +0900

    Design : 스택 뷰 안에 각 뷰를 넣어 회원가입페이지 버튼 제외 UI 구현

--- MegaStruct/Controller/JoinController.swift
+++ MegaStruct/Controller/JoinController.swift
@@ -8,22 +8,24 @@
 import UIKit
 
 class JoinController: UIViewController {
-
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+//        customTextField(_textField: userIdTextField)
+//        customTextField(_textField: userPWDTextField)
+        
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func customTextField(_textField: UITextField) {
+        //텍스트 필드 둥글게
+        _textField.layer.cornerRadius = 25.0
+        _textField.layer.masksToBounds = true
+        
+        //텍스트 필드 안에 패딩
+        _textField.leftView = UIView(frame: CGRect(x: 0.0, y: 0.0, width: 16.0, height: 0.0))
+        _textField.leftViewMode = .always
     }
-    */
-
 }
